d8a0fb5076805437053e38adf9f0638142885ec0
pkratz22 <51302875+pkratz22@users.noreply.github.com>
2020-07-25 22:17:45 -0400

    Added another test puzzle from different source

--- tests/tests.py
+++ tests/tests.py
@@ -6,9 +6,13 @@ class test_nonogram_from_image(unittest.TestCase):
     """Test cases for nonogram_from_image"""
     def test_get_image(self):
         self.assertIsNotNone(nonogram_from_image.get_image("tests/input_images/image1.jpg"))
+        self.assertIsNotNone(nonogram_from_image.get_image("tests/input_images/image2.jpg"))
 
     def test_get_puzzle_space(self):
         image = nonogram_from_image.get_image("tests/input_images/image1.jpg")
+        self.assertIsNotNone(nonogram_from_image.get_puzzle_space(image))
+
+        image = nonogram_from_image.get_image("tests/input_images/image2.jpg")
         self.assertIsNotNone(nonogram_from_image.get_puzzle_space(image))
 
 if __name__ == "__main__":
