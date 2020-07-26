eccf3b1809e4ef7dfc75be3628c3b51d4e66db7b
pkratz22 <51302875+pkratz22@users.noreply.github.com>
2020-07-25 22:20:24 -0400

    Formatting

--- nonogram_from_image/nonogram_from_image.py
+++ nonogram_from_image/nonogram_from_image.py
@@ -9,10 +9,10 @@ def get_image(path):
 
 def transform_image(image):
     """Transform image to detect edges"""
-    
+
     # make image gray
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
+
     # apply Gaussian Blur to reduce noise
     image_blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 
@@ -20,14 +20,15 @@ def transform_image(image):
     edges = cv2.Canny(image_blurred, 100, 300, apertureSize=3)
 
     # find contours
-    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(
+        edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     # finds largest contour
     c = max(contours, key=cv2.contourArea)
     x, y, w, h = cv2.boundingRect(c)
 
     # creates image with contour
-    cropped_image = image[y:y+h, x:x+w]
+    cropped_image = image[y:y + h, x:x + w]
 
     # write image
     cv2.imwrite("tests/output_images/edge.jpg", cropped_image)
@@ -39,6 +40,7 @@ def get_puzzle_space(img):
     """Given image, get puzzle area"""
     transformed_image = transform_image(img)
     return transformed_image
+
 
 if __name__ == "__main__":
     image_path = input("Please enter path to image file: ")
--- tests/tests.py
+++ tests/tests.py
@@ -4,9 +4,12 @@ from .context import nonogram_from_image
 
 class test_nonogram_from_image(unittest.TestCase):
     """Test cases for nonogram_from_image"""
+
     def test_get_image(self):
-        self.assertIsNotNone(nonogram_from_image.get_image("tests/input_images/image1.jpg"))
-        self.assertIsNotNone(nonogram_from_image.get_image("tests/input_images/image2.jpg"))
+        self.assertIsNotNone(nonogram_from_image.get_image(
+            "tests/input_images/image1.jpg"))
+        self.assertIsNotNone(nonogram_from_image.get_image(
+            "tests/input_images/image2.jpg"))
 
     def test_get_puzzle_space(self):
         image = nonogram_from_image.get_image("tests/input_images/image1.jpg")
@@ -14,6 +17,7 @@ class test_nonogram_from_image(unittest.TestCase):
 
         image = nonogram_from_image.get_image("tests/input_images/image2.jpg")
         self.assertIsNotNone(nonogram_from_image.get_puzzle_space(image))
+
 
 if __name__ == "__main__":
     unittest.main()
