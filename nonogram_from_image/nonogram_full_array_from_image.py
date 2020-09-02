966f51c849d53829a9aa8ceae87536f1347aefb2
pkratz22 <51302875+pkratz22@users.noreply.github.com>
2020-09-02 13:59:01 -0400

    Successfully counts number of color switches

--- nonogram_from_image/nonogram_full_array_from_image.py
+++ nonogram_from_image/nonogram_full_array_from_image.py
@@ -2,6 +2,7 @@
 # imports
 import os
 import cv2
+import numpy as np
 
 
 def get_image(path):
@@ -66,16 +67,20 @@ def get_individual_cell_dimensions(image):
     (_, black_white_image) = cv2.threshold(gray_image, 150, 255, cv2.THRESH_BINARY)
     cv2.imwrite('tests/output_images/bwimage.jpg', black_white_image)
 
+    kernel = np.ones((1, 5), np.uint8)
+    erosion = cv2.erode(black_white_image, kernel, iterations=1)
+    cv2.imwrite('tests/output_images/eimage.jpg', erosion)
+
     white = 255
     black = 0
-    temp_color = white
-    rows = black_white_image.shape[0]
-    cols = black_white_image.shape[1]
+    rows = erosion.shape[0]
+    cols = erosion.shape[1]
 
     for row in range(rows):
         count = 0
+        temp_color = erosion[row][0]
         for col in range(cols):
-            current_color = black_white_image[row][col]
+            current_color = erosion[row][col]
             if temp_color != current_color:
                 if temp_color == white:
                     temp_color = black
@@ -83,13 +88,25 @@ def get_individual_cell_dimensions(image):
                     temp_color = white
                 count += 1
         if count > 2:
+            pixel_row_for_col_counting = row + 1
             break
+
+    temp_color = black
+    count = 0
+    for col in range(cols):
+        current_color = erosion[pixel_row_for_col_counting][col]
+        if temp_color != current_color:
+            if temp_color == white:
+                temp_color = black
+            else:
+                temp_color = white
+            count += 1
     return count
 
 
 if __name__ == "__main__":
     #nonogram_image_path = input("Please enter path to image file: ")
-    NONOGRAM_IMAGE_PATH = "tests/input_images/image1.jpg"
+    NONOGRAM_IMAGE_PATH = "tests/input_images/image2.jpg"
     nonogram_image = get_image(NONOGRAM_IMAGE_PATH)
     nonogram_image_name = get_image_name(NONOGRAM_IMAGE_PATH)
     transformed_image = transform_image(nonogram_image)
