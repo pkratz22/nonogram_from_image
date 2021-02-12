a36078c26d7d528d89dec0484f5dcb9226560776
pkratz22 <pkratz22@gmail.com>
2021-02-12 00:59:28 -0500

    Basic formatting and docstrings

--- nonogram_from_image/nonogram_array_from_image.py
+++ nonogram_from_image/nonogram_array_from_image.py
@@ -139,7 +139,7 @@ def draw_improved_grid_lines(image, num_rows, num_cols):
 
 def check_cell_for_number(cell):
     """Checks cell for number"""
-    pytesseract.pytesseract.tesseract_cmd = "Tesseract-OCR/tesseract"
+    pytesseract.pytesseract.tesseract_cmd = 'Tesseract-OCR/tesseract'
     number = pytesseract.image_to_string(cell, config=CUSTOM_CONFIG)
     if number != "":
         number = int(number)
@@ -199,7 +199,7 @@ def fix_array(array):
         for row in array:
             for col in row:
                 print(str(col) + (2 - len(str(col))) * " " + "|", end=" ")
-            print("\n" + ((len(row) * 4) * "-") + "\n")
+            print("\n{arg}\n".format(arg=((len(row) * 4) * "-")))
         is_fixed = input("Enter Y if grid is correct. Else enter N. \n")
         if is_fixed.lower() == "y":
             fixed = True
@@ -233,12 +233,12 @@ def fix_array(array):
                 new_cell = int(new_cell)
             array[row - 1][int(col) - 1] = new_cell
         else:
-            print("Invalid input, will start again \n")
+            print('Invalid input, will start again \n')
     return array
 
 
 def get_num_rows_cols(array):
-    """Get number of rows that have col data"""
+    """Get number of rows that have col data."""
     row_number = 0
     is_last_row = False
     while not is_last_row:
@@ -248,59 +248,98 @@ def get_num_rows_cols(array):
 
 
 def check_if_last_row(array, row_number):
-    """Loop through row for get_num_rows_cols"""
-    for cell in range(len(array[row_number])):
-        if array[row_number][cell] != "":
-            if array[row_number + 1][cell] != "":
+    """Loop through row for get_num_rows_cols."""
+    for cell in enumerate(array[row_number]):
+        if array[row_number][cell] != '':
+            if array[row_number + 1][cell] != '':
                 return False
     return True
 
 
 def get_row_col_array(array, rows_skipped):
-    """Get col array from grid"""
+    """Get col array from grid."""
     ver_hor_array = []
     total_rows = len(array)
     for row in range(rows_skipped, total_rows):
         individual_row_array = []
-        for cell in range(len(array[row])):
-            if array[row][cell] != "":
+        for cell in enumerate(array[row]):
+            if array[row][cell] != '':
                 individual_row_array.append(array[row][cell])
         ver_hor_array.append(individual_row_array)
     return ver_hor_array
 
 
 def get_ver_hor_array_as_string(ver_hor_array):
-    """Gets vertical or horizontal array as string"""
+    """Get vertical or horizontal array as string.
+
+    Args:
+        ver_hor_array: either ver or hor array
+
+    Returns:
+        stringified version
+    """
     ver_hor_string = '['
     for row_num, _ in enumerate(ver_hor_array):
-        ver_hor_string += '['
-        for cell in range(len(ver_hor_array[row_num])):
-            ver_hor_string += str(ver_hor_array[row_num][cell]) + ','
+        ver_hor_string = '{arg}['.format(arg=ver_hor_string)
+        for cell in enumerate(ver_hor_array[row_num]):
+            ver_hor_string = '{verhorstr}{indstr},'.format(
+                verhorstr=ver_hor_string,
+                indstr=str(ver_hor_array[row_num][cell]),
+            )
         ver_hor_string = ver_hor_string[:-1]
-        ver_hor_string += '],'
+        ver_hor_string = '{str}],'.format(str=ver_hor_string)
     ver_hor_string = ver_hor_string[:-1]
-    ver_hor_string += ']'
-    return ver_hor_string
+    return '{str}]'.format(str=ver_hor_string)
 
 
 def get_teal_string(vertical, horizontal):
-    """Formats string for use in teal online tool"""
+    """Format string for use in teal online tool.
+
+    Args:
+        vertical: vertical axis array
+        horizontal: horizontal axis array
+
+    Returns:
+        Returns teal string for use online
+    """
     ver_string = get_ver_hor_array_as_string(vertical)
     hor_string = get_ver_hor_array_as_string(horizontal)
-    return '{"ver":' + ver_string + ',"hor":' + hor_string + '}'
+    beginning_str = '{"ver":'
+    mid_str = ',"hor":'
+    end_str = '}'
+    return '{begstr}{verarg}{midstr}{horarg}{endstr}'.format(
+        begstr=beginning_str,
+        midstr=mid_str,
+        endstr=end_str,
+        verarg=ver_string,
+        horarg=hor_string,
+    )
 
 
-def main():
-    """Main function"""
-    # nonogram_image_path = input("Please enter path to image file: ")
-    nonogram_image_path = "tests/input_images/image1.jpg"
+def main(image_path):
+    """Output array from image.
+
+    Args:
+        image_path: path to image
+
+    Returns:
+        teal string to be used online
+    """
+
+
+
+if __name__ == '__main__':
+    nonogram_image_path = 'tests/input_images/image1.jpg'
     nonogram_image = get_image(nonogram_image_path)
-    # nonogram_image_name = get_image_name(nonogram_image_path)
     transformed_image = transform_image(nonogram_image)
     number_of_rows, number_of_cols = get_num_rows_cols_from_image(
-        transformed_image)
+        transformed_image,
+    )
     finished_array = get_array_from_grid(
-        transformed_image, number_of_rows, number_of_cols)
+        transformed_image,
+        number_of_rows,
+        number_of_cols,
+    )
     grid_array = organize_array_by_rows(finished_array, number_of_cols)
     corrected_grid_array = fix_array(grid_array)
     transposed_array = list(map(list, zip(*corrected_grid_array)))
@@ -309,7 +348,3 @@ def main():
     ver_array = get_row_col_array(grid_array, num_cols)
     hor_array = get_row_col_array(transposed_array, num_rows)
     teal_string = get_teal_string(ver_array, hor_array)
-    return teal_string
-
-if __name__ == "__main__":
-    main()
